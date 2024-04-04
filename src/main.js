a0e67f6e306356c22d000006eab70a71099b6948
Oleh Kutafin <kutafin.o.v@gmail.com>
2024-04-03 18:41:12 -0700

    add home section

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import '../styles/normalize.css'
 import '../styles/style.css'
 import '../styles/components/header.css'
+import '../styles/components/mobile-nav.css'
+import '../styles/components/home.css'
+import '../styles/components/about.css'
 import '../styles/components/footer.css'
 import '../styles/utils.css'
+
+import mobileNav from '../src/ulils/mobile-nav.js'
+
+mobileNav();
